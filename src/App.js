4d35e6bb65826dc7023c67326f07f6922b1fef8a
Supakorn Thongtra <armm.supakorn@gmail.com>
2019-10-24 00:23:17 +0700

    add: liff v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const liff = window.liff
 
 class App extends Component {
+  state = {
+    name: '',
+    qrcode: ''
+  }
+  componentDidMount() {
+    liff.init(
+      { liffId: '1638080216-G7bd69Nk' },
+      async data => {
+        // Now you can call LIFF API
+        const userId = data.context.userId
+        console.log('userId: ', userId)
+        this.setState({ name: userId })
+        const profile = await liff.getProfile()
+        console.log('profile: ', profile)
+      },
+      err => {
+        console.log('err: ', err)
+        // LIFF initialization failed
+      }
+    )
+  }
+
+  sendMessage = () => {
+    liff
+      .sendMessages([
+        {
+          type: 'text',
+          text: 'Hello, World!'
+        }
+      ])
+      .then(() => {
+        console.log('message sent')
+      })
+      .catch(err => {
+        console.log('error', err)
+      })
+  }
+
+  scanCode = () => {
+    liff.scanCode().then(result => {
+      // e.g. result = { value: "Hello LIFF app!" }
+      const stringifiedResult = JSON.stringify(result);
+      this.setState({ qrcode: stringifiedResult })
+    }).catch(err => {
+      this.setState({ qrcode: 'scanCode failed!' })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -17,12 +67,21 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React 
           </a>
-        </header>
+        </header> */}
+        {this.state.name}
+        {this.state.qrcode}
+        <br />
+        <button onClick={this.sendMessage}>Send Message</button>
+        <button onClick={() => liff.closeWindow()}>Close</button>
+        <button onClick={this.scanCode}>Scan QRCode</button>
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+// https://developers.line.biz/en/docs/liff/developing-liff-apps/#sending-messages
